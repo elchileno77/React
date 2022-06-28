d0feb8ca63fc90894cb3e7d5407a29171a8c1b95
Richard Asenjo <richardasenjo2088@gmail.com>
2022-06-28 03:26:59 -0400

    Correccion ultimo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GifExpertApp } from '../../../React/04/vite-project/src/GifExpertApp'
 import { AddUser } from './components/AddUser'
 
 import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GifExpertApp /> 
      <hr></hr>
     <AddUser /> 
   </React.StrictMode>
